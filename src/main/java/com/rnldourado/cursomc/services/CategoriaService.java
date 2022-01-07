79b8abbb1f9b743475b7ba187e4b427891885213
Raniel <rnldourado@gmail.com>
2022-01-07 13:00:50 -0300

    Changes in endpoint /categorias/{id}

--- src/main/java/com/rnldourado/cursomc/services/CategoriaService.java
+++ src/main/java/com/rnldourado/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.rnldourado.cursomc.services;
 
 import com.rnldourado.cursomc.domain.Categoria;
 import com.rnldourado.cursomc.repositories.CategoriaRepository;
+import com.rnldourado.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +14,17 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository repo;
 
-    public Categoria buscar(Integer id){
+//    public Categoria buscar(Integer id){
+//        Optional<Categoria> obj = repo.findById(id);
+//        if (obj == null){
+//            throw new ObjectNotFoundException("Objeto não encontrado! Id: "+ id+"Tipo: "+Categoria.class.getName());
+//        }
+//        return obj.orElse(null);
+//    }
+
+    public Categoria buscar(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 }
