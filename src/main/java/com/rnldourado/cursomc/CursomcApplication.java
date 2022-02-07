a008f3f6a6e66d407336f7954d8b82971285c82d
Raniel <rnldourado@gmail.com>
2022-02-07 17:00:34 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/rnldourado/cursomc/CursomcApplication.java
+++ src/main/java/com/rnldourado/cursomc/CursomcApplication.java
@@ -1,8 +1,10 @@
 package com.rnldourado.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import com.rnldourado.cursomc.domain.*;
+import com.rnldourado.cursomc.domain.enums.EstadoPagamento;
 import com.rnldourado.cursomc.domain.enums.TipoCliente;
 import com.rnldourado.cursomc.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +32,12 @@ public class CursomcApplication implements CommandLineRunner {
 
     @Autowired
     private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
@@ -78,5 +86,22 @@ public class CursomcApplication implements CommandLineRunner {
         cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
         clienteRepository.saveAll(Arrays.asList(cli1));
         enderecoRepository.saveAll(Arrays.asList(e1,e2));
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+        Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+        Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+        Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+        ped1.setPagamento(pagto1);
+
+        Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+        ped2.setPagamento(pagto2);
+
+        cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+        pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+        pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
     }
 }
